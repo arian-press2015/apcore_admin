4bcdd750dc0ddff3a54d8b94aa6d0354f7600b9d
AP2015 <arian.press2015@gmail.com>
2024-07-01 00:33:45 +0330

    fix: fix user handler

--- cmd/users.go
+++ cmd/users.go
@@ -24,7 +24,7 @@ var usersCmd = &cobra.Command{
 }
 
 func getUsers(cfg *config.Config, httpClient *http.Client, tokenManager *token.TokenManager, offset int) {
-	url := fmt.Sprintf("%s/admin/users?offset=%d&limit=10", cfg.BackendURL, offset)
+	url := fmt.Sprintf("%s/users?offset=%d&limit=10", cfg.BackendURL, offset)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		fmt.Printf("Error creating request: %v\n", err)
@@ -50,15 +50,20 @@ func getUsers(cfg *config.Config, httpClient *http.Client, tokenManager *token.T
 		return
 	}
 
-	var users []User
-	err = json.NewDecoder(resp.Body).Decode(&users)
+	var responseBody struct {
+		Data    []User `json:"data"`
+		Message string `json:"message"`
+		TrackID string `json:"trackId"`
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&responseBody)
 	if err != nil {
 		fmt.Printf("Error decoding response: %v\n", err)
 		return
 	}
 
-	for _, user := range users {
-		fmt.Printf("ID: %s, Name: %s, Email: %s\n", user.ID, user.Name, user.Email)
+	for _, user := range responseBody.Data {
+		fmt.Printf("ID: %s, Name: %s, Phone: %s, Verified: %t\n", user.ID, user.FullName, user.Phone, user.Verified)
 	}
 
 	fmt.Println("Press 'n' for next page, 'p' for previous page, or any other key to exit.")
@@ -79,7 +84,26 @@ func getUsers(cfg *config.Config, httpClient *http.Client, tokenManager *token.T
 }
 
 type User struct {
-	ID    string `json:"id"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
+	ID            string       `json:"id"`
+	CreatedAt     string       `json:"created_at"`
+	UpdatedAt     string       `json:"updated_at"`
+	DeletedAt     *string      `json:"deleted_at"`
+	FullName      string       `json:"fullName"`
+	Phone         string       `json:"phone"`
+	ProfileImage  string       `json:"profile_image"`
+	Nid           string       `json:"nid"`
+	Verified      bool         `json:"verified"`
+	Roles         []string     `json:"roles"`
+	Notifications []string     `json:"notifications"`
+	Subscription  Subscription `json:"subscription"`
+}
+
+type Subscription struct {
+	ID          string  `json:"id"`
+	CreatedAt   string  `json:"created_at"`
+	UpdatedAt   string  `json:"updated_at"`
+	DeletedAt   *string `json:"deleted_at"`
+	UserID      string  `json:"userID"`
+	Method      string  `json:"method"`
+	SubjectType string  `json:"subjectType"`
 }
